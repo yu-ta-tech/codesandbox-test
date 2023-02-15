2912244f7a097bef2e95c1fe405881cfe2339c78
yu-ta-tech <110362427+yu-ta-tech@users.noreply.github.com>
2023-02-15 22:57:58 +0900

    2/15

--- src/index.js
+++ src/index.js
@@ -100,7 +100,7 @@
 // const message2 = `名前は${name}です。年齢は${age}歳です。`;
 // console.log(message2);
 
-// const myProfile = ["じゃけぇ", 28];
+const myProfile = ["じゃけぇ", 28];
 
 // const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
 // console.log(message3);
